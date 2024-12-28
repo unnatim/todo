37e6e457c7796be613df73ebaff6ba50538c998e
Unnati Matai (Devops <GV0734@ZKGI.COM>
2024-12-29 00:18:56 +0530

    changes made to index file

--- src/test/index.js
+++ src/test/index.js
@@ -1,12 +1,10 @@
 const { expect } = require('chai');
-const { agent } = require('supertest');
+const request = require('supertest'); // Directly use supertest
 const app = require('../app');
 
-const request = agent;
-
-describe('Some controller', () => {
+describe('v1', () => {
   it('Get request to /test returns some text', async () => {
-    const res = await request(app).get('/test');
+    const res = await request(app).get('/test'); // Call request with app
     const textResponse = res.body;
     expect(res.status).to.equal(200);
     expect(textResponse.text).to.be.a('string');
